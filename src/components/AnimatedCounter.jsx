84c6598c6faba92768079d6557ee17758d1d3730
Marco Ng <yautin.ng8@gmail.com>
2026-07-05 23:16:46 +0800

    improved overall design and refactored

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { counterItems } from "../constants";
 
